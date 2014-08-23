e893ef58590c9f9b25bfff7dbc8e5ebb75f1b7bc
NiavlysB <sylvain.brunerie@gmail.com>
2014-08-23 19:40:12 +0200

    Génération de terrain ok

--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
 	t.window.title = "Two In One - Niavlys - Ludum Dare 30"
 	t.window.fullscreentype = "desktop"
-	t.window.fullscreen = true
+	--t.window.fullscreen = true
 end
